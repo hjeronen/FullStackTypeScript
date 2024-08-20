db267a38ba1bfeed6f851430af542b7266a65140
Heli Eronen <herojonen@gmail.com>
2024-08-20 17:38:36 +0300

    Exercise 9.10

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import cors from 'cors';
+import diagnosesRouter from './routes/diagnoses';
 const app = express();
 app.use(express.json());
 
-const cors = require('cors');
 app.use(cors());
 
 const PORT = 3000;
@@ -11,6 +12,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
