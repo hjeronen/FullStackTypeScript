75abd92a2f8975f175cf83842e4f69ac9729dc0d
Heli Eronen <herojonen@gmail.com>
2024-10-24 12:24:59 +0300

    Exercise 9.12

--- patientor-backend/src/services/patientService.ts
+++ patientor-backend/src/services/patientService.ts
@@ -1,5 +1,6 @@
-import { NonSensitivePatientInfo } from '../types';
+import { NonSensitivePatientInfo, Patient } from '../types';
 import patientData from '../../data/patients';
+import { v1 as uuid } from 'uuid';
 
 const getPatients = (): NonSensitivePatientInfo[] => {
   return patientData.map(({ id, name, dateOfBirth, gender, occupation }) => ({
@@ -11,4 +12,24 @@ const getPatients = (): NonSensitivePatientInfo[] => {
   }));
 };
 
-export default { getPatients };
+const addPatient = (
+  name: string,
+  dateOfBirth: string,
+  ssn: string,
+  gender: string,
+  occupation: string
+): Patient => {
+  const newPatient: Patient = {
+    id: uuid(),
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation,
+  };
+
+  patientData.push(newPatient);
+  return newPatient;
+};
+
+export default { getPatients, addPatient };
