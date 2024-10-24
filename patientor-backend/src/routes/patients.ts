85855293bda1cbad78049a5e52dd137d97c3064b
Heli Eronen <herojonen@gmail.com>
2024-10-24 17:31:22 +0300

    Exercise 9.14

--- patientor-backend/src/routes/patients.ts
+++ patientor-backend/src/routes/patients.ts
@@ -1,25 +1,50 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import patientService from '../services/patientService';
-import toNewPatient from '../utils';
+import { z } from 'zod';
+import { NewPatientSchema } from '../utils';
+import { NewPatient, Patient } from '../types';
 
 const router = express.Router();
+
+export const newPatientParser = (
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
+  try {
+    NewPatientSchema.parse(req.body);
+    next();
+  } catch (error: unknown) {
+    next(error);
+  }
+};
+
+const errorMiddleware = (
+  error: unknown,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (error instanceof z.ZodError) {
+    res.status(400).send({ error: error.issues });
+  } else {
+    next(error);
+  }
+};
 
 router.get('/', (_req, res) => {
   res.send(patientService.getPatients());
 });
 
-router.post('/', (req, res) => {
-  try {
-    const newPatient = toNewPatient(req.body);
-    const addedPatient = patientService.addPatient(newPatient);
+router.post(
+  '/',
+  newPatientParser,
+  (req: Request<unknown, unknown, NewPatient>, res: Response<Patient>) => {
+    const addedPatient = patientService.addPatient(req.body);
     res.json(addedPatient);
-  } catch (error: unknown) {
-    let errorMessage = 'Something went wrong.';
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    res.status(400).send(errorMessage);
   }
-});
+);
+
+router.use(errorMiddleware);
 
 export default router;
--- patientor-backend/src/utils.ts
+++ patientor-backend/src/utils.ts
@@ -1,78 +1,14 @@
+import { z } from 'zod';
 import { Gender, NewPatient } from './types';
 
-const toNewPatient = (object: unknown): NewPatient => {
-  if (!object || typeof object !== 'object') {
-    throw new Error('Incorrect or missing data');
-  }
+export const NewPatientSchema = z.object({
+  name: z.string(),
+  dateOfBirth: z.string().date(),
+  ssn: z.string(),
+  gender: z.nativeEnum(Gender),
+  occupation: z.string(),
+});
 
-  if (
-    'name' in object &&
-    'dateOfBirth' in object &&
-    'ssn' in object &&
-    'gender' in object &&
-    'occupation' in object
-  ) {
-    const newPatient: NewPatient = {
-      name: parseName(object.name),
-      dateOfBirth: parseDateOfBirth(object.dateOfBirth),
-      ssn: parseSsn(object.ssn),
-      gender: parseGender(object.gender),
-      occupation: parseOccupation(object.occupation),
-    };
-
-    return newPatient;
-  }
-
-  throw new Error('Incorrect data: some fields are missing');
+export const toNewPatient = (object: unknown): NewPatient => {
+  return NewPatientSchema.parse(object);
 };
-
-const isString = (text: unknown): text is string => {
-  return typeof text === 'string' || text instanceof String;
-};
-
-const parseName = (name: unknown): string => {
-  if (!name || !isString(name)) {
-    throw new Error('Incorrect or missing name');
-  }
-  return name;
-};
-
-const isDate = (date: string): boolean => {
-  return Boolean(Date.parse(date));
-};
-
-const parseDateOfBirth = (date: unknown): string => {
-  if (!date || !isString(date) || !isDate(date)) {
-    throw new Error('Incorrect or missing date: ' + date);
-  }
-  return date;
-};
-
-const parseSsn = (ssn: unknown): string => {
-  if (!ssn || !isString(ssn)) {
-    throw new Error('Incorrect or missing ssn');
-  }
-  return ssn;
-};
-
-const parseOccupation = (occupation: unknown): string => {
-  if (!occupation || !isString(occupation)) {
-    throw new Error('Incorrect or missing occupation');
-  }
-  return occupation;
-};
-
-const isGender = (param: string): param is Gender => {
-  return Object.values(Gender)
-    .map((v) => v.toString())
-    .includes(param);
-};
-
-const parseGender = (gender: unknown): Gender => {
-  if (!gender || !isString(gender) || !isGender(gender)) {
-    throw new Error('Incorrect or missing gender: ' + gender);
-  }
-  return gender;
-};
-
-export default toNewPatient;
