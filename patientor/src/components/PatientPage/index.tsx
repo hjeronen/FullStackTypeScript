930d007e4a45a8e106365df453a4cb6f2fe8d2a5
Heli Eronen <herojonen@gmail.com>
2024-10-28 10:52:46 +0200

    Exercise 9.22

---
+++ patientor/src/components/PatientPage/index.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { Gender, Patient } from "../../types";
+import { useMatch } from "react-router-dom";
+import { Female, Male, Transgender } from "@mui/icons-material";
+import axios from "axios";
+import patientService from "../../services/patients";
+
+const PatientPage = () => {
+  const [patient, setPatient] = useState<Patient | undefined>(undefined);
+  const [error, setError] = useState<string | undefined>("");
+  const match = useMatch("/:id");
+
+  useEffect(() => {
+    if (match?.params.id) {
+      patientService
+        .getPatient(match.params.id)
+        .then((fetched) => setPatient(fetched))
+        .catch((error: unknown) => {
+          if (axios.isAxiosError(error) && error.response) {
+            setError(error.response.data.error);
+          } else {
+            setError("Something went wrong.");
+          }
+        });
+    } else {
+      setPatient(undefined);
+    }
+  }, [match]);
+
+  const getGenderIcon = () => {
+    switch (patient?.gender) {
+      case Gender.Male:
+        return <Male />;
+      case Gender.Female:
+        return <Female />;
+      case Gender.Other:
+        return <Transgender />;
+      default:
+        null;
+    }
+  };
+
+  return patient ? (
+    <div>
+      <h1>
+        {patient.name} {getGenderIcon()}
+      </h1>
+      <div>ssn: {patient.ssn}</div>
+      <div>occupation: {patient.occupation}</div>
+    </div>
+  ) : (
+    <div>{error ?? "No patient data available."}</div>
+  );
+};
+
+export default PatientPage;
